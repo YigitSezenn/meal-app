b235b8a5ddb715d0615d977a72c441d91c86f65c
yigit <ysezenn@outlook.com>
2026-07-25 22:07:22 +0300

    finish

--- components/CategoryGridTitle.js
+++ components/CategoryGridTitle.js
@@ -1,14 +1,15 @@
-import { Pressable,View,Text ,StyleSheet} from "react-native";
+import { Pressable,View,Text ,StyleSheet,Platform} from "react-native";
 
 
-function CategoryGridTitle({ title,color }) {
+function CategoryGridTitle({ title,color ,onPress}) {
   return (
   
     <View style={[styles.gridItem]}>
-       <Pressable style={styles.button}>
-      <View style={[styles.innerContainer]}>
+       <Pressable android_ripple={{color:'#ccc'}}  style={ ({pressed}) => [styles.button, pressed ? styles.buttonPressed : null]}   onPress={onPress}>
+      <View style={[styles.innerContainer, {backgroundColor: color}]}>
         <Text>{title}</Text>
       </View>
+      
        </Pressable>
     </View>
   
@@ -29,6 +30,7 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.25, // shadow opacity for iOS
     shadowOffset: { width: 0, height: 2 }, // shadow offset for iOS
     shadowRadius: 8, // shadow radius for iOS
+    overflow: Platform.OS === 'ios' ? 'hidden' : 'visible', // clip children to the rounded corners
 
   },
   
@@ -36,9 +38,13 @@ const styles = StyleSheet.create({
 
     flex: 1,
   },
+  buttonPressed: {
+    opacity: 0.5,
+  },
   innerContainer: {
     flex: 1,
     padding: 16,
+    borderRadius: 8,
     justifyContent: 'center',
     alignItems: 'center',
 
